7bff985bff248882906b4afad94a89eb9e077022
dewafer <dewafer@gmail.com>
2020-01-19 17:14:54 +0800

    添加科学安装及中文说明

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Play with k8s clusters
 ------
 
-## Requirements
+## Prerequisites
 
 * [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
 * [Virtualbox](https://www.virtualbox.org)
@@ -73,19 +73,75 @@ So please use the following orders to startup the clusters.
 
 1. Start Vms using Vagrant
 ```cmd
-$ vagrant up
+> vagrant up
 ```
 
 2. Provision kuberneters using Vagrant provision.
 ```cmd
-$ vagrant provision --provision-with kubernetes
+> vagrant provision --provision-with kubernetes
 ```
 
 3. Use your k8s clusters.
 
 4. You can also run Ansible playbook after ssh into the master node, the project folder will be automatically mounted to `/vagrant`, and Ansible should already be installed.
 ```cmd
-$ vagrant ssh k8s-master-01
+> vagrant ssh k8s-master-01
 > cd /vagrant
 > ansible-playbook -i inventory dashboard_token.yml
+```
+
+## Memo for myself
+
+* If you can not connect to k8s-master-01, add router for interface `vboxnet1` of virtualbox:
+```bash
+> sudo route add -net 192.168.10 -interface vboxnet1
+# check route
+> netstat -nr
+```
+
+
+# 中文安装指南
+
+## 事先安装
+
+* [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/) (可不装)
+* [Virtualbox](https://www.virtualbox.org)（注意请安装**6.0.x**的版本！）
+* [Vagrant](https://www.vagrantup.com)
+* [Ansible](https://www.ansible.com) （可不装，不支持Windows）
+
+**注意1：当前 Vagrant 版本（2.2.6）不支持 Virtualbox 6.1，请安装6.0.x版本的 VirtualBox**
+
+**注意2：Ansible 不支持使用 Windows 做主机，如果你是 Windows 用户，可以不用装，当然你是 Macos 或者 Linux 用户也可以不用装，我都帮你们在 Vagrant Provision 里配置好了。**
+
+## 安装步骤
+
+1. 使用 Vagrant 起虚拟机：
+```bash
+> vagrant up
+```
+
+2. 国内网络环境用户、不能科学上网的国内用户，我特地为大家准备了使用阿里云镜像的加速；如果你能科学上网或者身处国外网络环境，请跳过此步骤：
+```bash
+> vagrant provision --provision-with aliyun_mirror
+```
+
+3. 安装 Kubernetes
+```bash
+> vagrant provision --provision-with kubernetes
+```
+
+4. 狗定，大家都是资深开发/运维人员了，自己研究吧。
+
+## 常见问题
+
+### 国内网络环境 / 非科学上网环境 `vagrant up` 慢、超时报错等
+
+1. 使用此链接下载ubuntu的box
+```
+https://vagrantcloud.com/ubuntu/boxes/bionic64/versions/20200115.0.0/providers/virtualbox.box
+```
+
+2. 将下载的box添加到本地 Vagrant box ，记得自行替换路径
+```bash
+> vagrant box add --name ubuntu/bionic64 C:\\迅雷下载\\virtualbox.box
 ```
--- files/apply_aliyun_mirrors.sh
+++ files/apply_aliyun_mirrors.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Apply Aliyun apt mirrors..."
+echo "** 应用阿里云镜像..."
 
 sudo sed -i "s/archive.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list
 sudo apt-get update
 
 
-echo "Install python-pip"
+echo "** 安装 python-pip"
 
 sudo apt-get -y install python-pip
 
-echo "Apply Aliyun pip mirrors..."
+echo "** 应用阿里云 pip 镜像..."
 #pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 sudo touch /etc/pip.conf
 sudo bash -c 'cat <<EOF > /etc/pip.conf
@@ -21,7 +21,7 @@ index-url = https://mirrors.aliyun.com/pypi/simple/
 trusted-host=mirrors.aliyun.com
 EOF'
 
-echo "Install docker from Aliyun mirrors..."
+echo "** 从阿里云镜像安装 Docker..."
 
 # step 1: 安装必要的一些系统工具
 sudo apt-get update
@@ -29,22 +29,39 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 # step 2: 安装GPG证书
 curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
 # Step 3: 写入软件源信息
-sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+DOCKER_APT_SRC="deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "$DOCKER_APT_SRC"
 # Step 4: 更新并安装Docker-CE
 sudo apt-get -y update
-sudo apt-get -y install docker-ce=5:18.09.9~3-0~ubuntu-bionic
+#sudo apt-get -y install docker-ce=5:18.09.9~3-0~ubuntu-bionic
 
-docker version
+#docker version
 
-echo "Install Kubernetes from Aliyun mirrors..."
+echo "** 从阿里云镜像安装 Kubernetes..."
 
 sudo apt-get update 
 sudo apt-get install -y apt-transport-https
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add - 
-sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
-EOF'
+KUBERNETES_APT_SRC="deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main"
+sudo bash -c "cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+$KUBERNETES_APT_SRC
+EOF"
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+#sudo apt-get install -y kubelet kubeadm kubectl
 
-echo "DONE."
+echo "** Generate extra var file to override repo in kubernetes playbook..."
+sudo bash -c "cat <<EOF >/vagrant/vars/tmp
+---
+docker_apt_ignore_key_error: true
+docker_apt_repository: '$DOCKER_APT_SRC'
+kubernetes_apt_ignore_key_error: true
+kubernetes_apt_repository: '$KUBERNETES_APT_SRC'
+kubernetes_kubeadm_init_extra_opts: '--image-repository registry.aliyuncs.com/google_containers'
+EOF"
+
+echo "** export EXTRA_VAR_FILE=/vagrant/vars/tmp"
+sudo bash -c 'cat <<EOF >>/etc/environment
+EXTRA_VAR_FILE=/vagrant/vars/tmp
+EOF'
+
+echo "** DONE."
